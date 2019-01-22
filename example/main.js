d1f136e220fb3fc8f94f5c1f9a5822709255aefc
Igor Olshevsky <igor@adlab.pro>
2019-01-22 19:43:26 +0200

    test extension

--- example/main.js
+++ example/main.js
@@ -1,1 +1,11 @@
-console.log("FORCE PUBLISH!");
+const cde = require('cdecode/client');
+
+console.log("FORCE PUBLISH! API: ", cde);
+module.exports = {
+    activate: () => {
+        console.log("ACTIVATING! API: ", cde);
+    },
+    deactivate: () => {
+        console.log("DEACTIVATING :(");
+    },
+};
