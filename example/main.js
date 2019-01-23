d1d2f5426db40f0663bda77c9cd70467e66e22e3
Igor Olshevsky <igor@adlab.pro>
2019-01-23 16:45:56 +0200

    extensions

--- example/main.js
+++ example/main.js
@@ -3,6 +3,13 @@ const cde = require('cdecode/client');
 console.log("FORCE PUBLISH! API: ", cde);
 module.exports = {
     activate: () => {
+        const query = cde.gql`{ dummy }`;
+        console.log('QUERY: ', query);
+
+        cde.apollo.query({ query })
+            .then(data => console.log('APOLLO: ', data))
+            .catch(data => console.log('APOLLO.ERROR: ', data));
+
         console.log("ACTIVATING! API: ", cde);
     },
     deactivate: () => {
